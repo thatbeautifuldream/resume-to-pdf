fc138b839f7a4c1519239fe0df401cac44024658
Milind Mishra <milind.mishra4@gmail.com>
2025-09-27 07:45:13 +0530

    feat: update script for expanded and normal res

--- src/resume-to-pdf.js
+++ src/resume-to-pdf.js
@@ -2,26 +2,47 @@ import puppeteer from "puppeteer";
 import { mkdir } from "fs/promises";
 import { dirname } from "path";
 
-const RESUME_URL = "https://resume.milind.app/?expand=true";
+const RESUME_URLS = {
+  normal: "https://resume.milind.app",
+  expanded: "https://resume.milind.app/?expand=true"
+};
 
 export async function printResumeToPdf() {
   const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
-
-  await page.goto(RESUME_URL, {
-    waitUntil: "networkidle0",
-  });
 
   const currentYear = new Intl.DateTimeFormat("en", { year: "numeric" }).format(
     new Date()
   );
 
-  const FILE_PATH = `dist/Milind_Mishra_Resume_${currentYear}.pdf`;
+  const normalPage = await browser.newPage();
+  await normalPage.goto(RESUME_URLS.normal, {
+    waitUntil: "networkidle0",
+  });
 
-  await mkdir(dirname(FILE_PATH), { recursive: true });
+  const normalFilePath = `dist/Milind_Mishra_Resume_${currentYear}.pdf`;
+  await mkdir(dirname(normalFilePath), { recursive: true });
 
-  await page.pdf({
-    path: FILE_PATH,
+  await normalPage.pdf({
+    path: normalFilePath,
+    format: "A4",
+    printBackground: true,
+    margin: {
+      top: "1cm",
+      bottom: "1cm",
+      left: "1cm",
+      right: "1cm",
+    },
+  });
+
+  const expandedPage = await browser.newPage();
+  await expandedPage.goto(RESUME_URLS.expanded, {
+    waitUntil: "networkidle0",
+  });
+
+  const expandedFilePath = `dist/Milind_Mishra_Full_Resume_${currentYear}.pdf`;
+
+  await expandedPage.pdf({
+    path: expandedFilePath,
     format: "A4",
     printBackground: true,
     margin: {
